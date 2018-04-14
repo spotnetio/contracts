07374732c29f90f1f78a645e7d478eb5d6db2238
Marat Bunyatov <marat@Marats-MBP.home>
2018-04-14 00:46:17 -0400

    lock

--- contracts/Spot.sol
+++ contracts/Spot.sol
@@ -7,8 +7,10 @@ contract Spot is DSMath{
 	struct lock{
 		address		lender;
 		address		borrower;
+		address 	x1;
 		uint 		amountX1;
 		uint		amountEther; 
+		uint 		createdAt;
 	}
 	mapping (address => lock[]) locks;
 
@@ -17,9 +19,9 @@ contract Spot is DSMath{
 	event Log(uint a);
 
 	function Lock(
-		address token, 
 		address lender, 
-		address	borrower,	// WETH address
+		address	borrower,
+		address token, 	
 		uint	ethAmount,
 		uint 	rate,		// times 100
 		uint 	fee,		// basis (10000)
@@ -28,148 +30,180 @@ contract Spot is DSMath{
 		require(rate != 0);
 		require(ethAmount > 0);
 		uint x1Amount = 1000000*ethAmount/(rate*(fee + margin));
-		locks[token].push(lock(
+		lock memory lk = lock(
 			lender, 
 			borrower, 
+			token,
 			x1Amount, 
-			ethAmount
-		));
+			ethAmount,
+			now
+		);
+		locks[lender].push(lk);
+		locks[borrower].push(lk);
 		// Transfer WETH from borrower to this		
-		ERC20(WEth).transferFrom(
-			borrower, 
-			this,
-			ethAmount
-		);
-		// Transfer base to borrower
-		ERC20(token).transferFrom(
-			lender,
-			borrower, 
-			x1Amount
-		);
+		// ERC20(WEth).transferFrom(
+		// 	borrower, 
+		// 	this,
+		// 	ethAmount
+		// );
+		// // Transfer base to borrower
+		// ERC20(token).transferFrom(
+		// 	lender,
+		// 	borrower, 
+		// 	x1Amount
+		// );
 	}
 	
-	function BuyToCover(
-		address token, 
-		address borrower,
-		uint 	rate,		// percent 	(100)
-		uint 	fee,		// basis 	(10000)
-		uint 	margin		// basis	(10000)
-	) public {
-		lock[] storage locksForToken = locks[token];
-		for (uint i=0; i<locksForToken.length; i++) {
-			if (locksForToken[i].borrower == msg.sender) {
-				require (ERC20(token).allowance(msg.sender, this) >= locksForToken[i].amountX1);
-				ERC20(token).transferFrom(
-					msg.sender, 
-					locksForToken[i].lender, 
-					locksForToken[i].amountX1
-				);
-				// Transfer WETH to borrower from this		
-				ERC20(WEth).transfer(
-					borrower, 
-					locksForToken[i].amountX1*rate*(fee + margin)/1000000
-				);
-				delete locksForToken[i]; // TODO: implement linked list
-			}
-		}
-	}
+	// function BuyToCover(
+	// 	address token, 
+	// 	address borrower,
+	// 	uint 	rate,		// percent 	(100)
+	// 	uint 	fee,		// basis 	(10000)
+	// 	uint 	margin		// basis	(10000)
+	// ) public {
+	// 	lock[] storage locksForToken = locks[token];
+	// 	for (uint i=0; i<locksForToken.length; i++) {
+	// 		if (locksForToken[i].borrower == msg.sender) {
+	// 			require (ERC20(token).allowance(msg.sender, this) >= locksForToken[i].amountX1);
+	// 			ERC20(token).transferFrom(
+	// 				msg.sender, 
+	// 				locksForToken[i].lender, 
+	// 				locksForToken[i].amountX1
+	// 			);
+	// 			// Transfer WETH to borrower from this		
+	// 			ERC20(WEth).transfer(
+	// 				borrower, 
+	// 				locksForToken[i].amountX1*rate*(fee + margin)/1000000
+	// 			);
+	// 			delete locksForToken[i]; // TODO: implement linked list
+	// 		}
+	// 	}
+	// }
 
-	function SwapLenders(
-		address 	token, 
-		address		lender,
-		address[]	lenders,
-		uint 		rate,		// percent 	(100)
-		uint 		fee,		// basis 	(10000)
-		uint 		margin		// basis		(10000)) 
-	) public {
-		lock[] storage locksForToken = locks[token];
-		for (uint i=0; i<locksForToken.length; i++) {
-			if (locksForToken[i].lender == lender) {
-				for (uint j=0; j<lenders.length; j++) {
-					uint allowance = ERC20(token).allowance(
-						lenders[j], 
-						this
-					);
-					if (allowance > 0) {
-						uint x1ToTransfer = min(allowance, locksForToken[i].amountX1);
-						uint etherToTransfer = x1ToTransfer*rate*(fee + margin)/1000000;
-						locksForToken[i].amountX1 -= x1ToTransfer;
-						locksForToken[i].amountEther -= etherToTransfer;
-						locks[token].push(lock(
-							lenders[j], 
-							locksForToken[i].borrower, 
-							x1ToTransfer, 
-							etherToTransfer
-						));
-						// Transfer X1 from new lender to old
-						ERC20(token).transferFrom(
-							lenders[j], 
-							lender, 
-							x1ToTransfer
-						);
-					}
-				}
-			}
-		}
-	}
+	// function SwapLenders(
+	// 	address 	token, 
+	// 	address		lender,
+	// 	address[]	lenders,
+	// 	uint 		rate,		// percent 	(100)
+	// 	uint 		fee,		// basis 	(10000)
+	// 	uint 		margin		// basis		(10000)) 
+	// ) public {
+	// 	lock[] storage locksForToken = locks[token];
+	// 	for (uint i=0; i<locksForToken.length; i++) {
+	// 		if (locksForToken[i].lender == lender) {
+	// 			for (uint j=0; j<lenders.length; j++) {
+	// 				uint allowance = ERC20(token).allowance(
+	// 					lenders[j], 
+	// 					this
+	// 				);
+	// 				if (allowance > 0) {
+	// 					uint x1ToTransfer = min(allowance, locksForToken[i].amountX1);
+	// 					uint etherToTransfer = x1ToTransfer*rate*(fee + margin)/1000000;
+	// 					locksForToken[i].amountX1 -= x1ToTransfer;
+	// 					locksForToken[i].amountEther -= etherToTransfer;
+	// 					locks[token].push(lock(
+	// 						lenders[j], 
+	// 						locksForToken[i].borrower, 
+	// 						x1ToTransfer, 
+	// 						etherToTransfer
+	// 					));
+	// 					// Transfer X1 from new lender to old
+	// 					ERC20(token).transferFrom(
+	// 						lenders[j], 
+	// 						lender, 
+	// 						x1ToTransfer
+	// 					);
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-	function MarginCall(
-		address token,
-		address borrower,
-		bytes32 r, 
-		bytes32 s,
-		uint8 	v, 
-		uint 	rate,		// percent 	(100)
-		uint 	fee,		// basis 	(10000)
-		uint 	margin		// basis	(10000)) 
-	) public {
-		bytes32 message = keccak256(rate + fee + margin);
-		require(ecrecover(message, v, r, s) == address(0x0)); // Oracle address here
-		lock[] storage locksForToken = locks[token];
-		for (uint i=0; i<locksForToken.length; i++) {
-			if (locksForToken[i].borrower == borrower) {
-				if(locksForToken[i].amountEther < locksForToken[i].amountX1*rate*(fee + margin)/1000000) {
-					// Send Ether to lender
-					ERC20(WEth).transfer(
-						locksForToken[i].lender, 
-						locksForToken[i].amountEther
-					);
-					delete locksForToken[i];
-				}
-			}
-		}
+	// function MarginCall(
+	// 	address token,
+	// 	address borrower,
+	// 	bytes32 r, 
+	// 	bytes32 s,
+	// 	uint8 	v, 
+	// 	uint 	rate,		// percent 	(100)
+	// 	uint 	fee,		// basis 	(10000)
+	// 	uint 	margin		// basis	(10000)) 
+	// ) public {
+	// 	bytes32 message = keccak256(rate + fee + margin);
+	// 	require(ecrecover(message, v, r, s) == address(0x0)); // Oracle address here
+	// 	lock[] storage locksForToken = locks[token];
+	// 	for (uint i=0; i<locksForToken.length; i++) {
+	// 		if (locksForToken[i].borrower == borrower) {
+	// 			if(locksForToken[i].amountEther < locksForToken[i].amountX1*rate*(fee + margin)/1000000) {
+	// 				// Send Ether to lender
+	// 				ERC20(WEth).transfer(
+	// 					locksForToken[i].lender, 
+	// 					locksForToken[i].amountEther
+	// 				);
+	// 				delete locksForToken[i];
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	function getNumberOfLocksForAddress(
+		address user
+	) public view returns (uint) {
+		return locks[user].length;
 	}
 
 	function getLock(
-		address token, 
-		address lender, 
-		address borrower
-	) public view returns (uint, uint) {
-		uint x1Amount;
-		uint etherAmount;
-		lock[] storage locksForToken = locks[token];
-		for (uint i=0; i<locksForToken.length; i++) {
-			if (locksForToken[i].lender == lender && locksForToken[i].borrower == borrower) {
-				x1Amount += locksForToken[i].amountX1;
-				etherAmount += locksForToken[i].amountEther;
-			}
-		}
-		return (x1Amount, etherAmount);
+		address user,
+		uint 	idx
+	) public view returns (
+		address, 
+		address, 
+		address, 
+		uint, 
+		uint, 
+		uint
+	) {
+		lock memory lk = locks[user][idx];
+		return (
+			lk.lender, 
+			lk.borrower,
+			lk.x1, 
+			lk.amountX1, 
+			lk.amountEther, 
+			lk.createdAt 
+		);
 	}
 
-	function getLockLender(
-		address token, 
-		address lender
-	) public view returns (uint) {
-		uint x1Amount;
-		lock[] storage locksForToken = locks[token];
-		for (uint i=0; i<locksForToken.length; i++) {
-			if (locksForToken[i].lender == lender) {
-				x1Amount += locksForToken[i].amountX1;
-			}
-		}
-		return x1Amount;
-	}
+	// function getLock(
+	// 	address token, 
+	// 	address lender, 
+	// 	address borrower
+	// ) public view returns (uint, uint) {
+	// 	uint x1Amount;
+	// 	uint etherAmount;
+	// 	lock[] storage locksForToken = locks[token];
+	// 	for (uint i=0; i<locksForToken.length; i++) {
+	// 		if (locksForToken[i].lender == lender && locksForToken[i].borrower == borrower) {
+	// 			x1Amount += locksForToken[i].amountX1;
+	// 			etherAmount += locksForToken[i].amountEther;
+	// 		}
+	// 	}
+	// 	return (x1Amount, etherAmount);
+	// }
+
+	// function getLockLender(
+	// 	address token, 
+	// 	address lender
+	// ) public view returns (uint) {
+	// 	uint x1Amount;
+	// 	lock[] storage locksForToken = locks[token];
+	// 	for (uint i=0; i<locksForToken.length; i++) {
+	// 		if (locksForToken[i].lender == lender) {
+	// 			x1Amount += locksForToken[i].amountX1;
+	// 		}
+	// 	}
+	// 	return x1Amount;
+	// }
 }
 
 
--- test/SpotHappyPath.js
+++ test/SpotHappyPath.js
@@ -1,52 +1,63 @@
 var Spot = artifacts.require("./Spot.sol");
-var X1Token = artifacts.require("./X1Token.sol");
+var EosToken = artifacts.require("./EosToken.sol");
+var WethToken = artifacts.require("./WethToken.sol");
 
 
 function log(obj) {
 	console.log(obj);
 }
 
-contract('Approve 10 X1 tokens, lock and buy to cover', function(accounts){
+contract('Approve 10 EOS & Weth tokens, lock and buy to cover', function(accounts){
 	var lender1 = accounts[0];
 	var borrower1 = accounts[1];
 	var lender2 = accounts[2];
 	var borrower2 = accounts[3];
 	var spotInstance;
-	var x1TokenInstance;
+	var eosTokenInstance;
+	var wethTokenInstance;
 
-	it("should approve spotnet for 10 X1 tokens", function() {
-		return X1Token.deployed().then(function(instance) {
-			x1TokenInstance = instance;
-			log(x1TokenInstance.address);
+	it("should approve spotnet for 10 EOS tokens", function() {
+		return EosToken.deployed().then(function(instance) {
+			eosTokenInstance = instance;
+			log(eosTokenInstance.address);
 			return Spot.deployed();
 		}).then(function(instance) {
 			spotInstance = instance;
-			x1TokenInstance.approve(spotInstance.address, 10, {from: lender1});
-			return x1TokenInstance.allowance(lender1, spotInstance.address);
+			return WethToken.deployed();
+		}).then(function(instance) {
+			wethTokenInstance = instance;
+			eosTokenInstance.approve(spotInstance.address, 10, {from: lender1});
+			return eosTokenInstance.allowance(lender1, spotInstance.address);
 		}).then(function(data) {
-			assert.equal(data, 10, "allowance amount should be " + 10);
+			assert.equal(data, 10, "eos allowance amount should be " + 10);
+			wethTokenInstance.approve(spotInstance.address, 10, {from: borrower1});
+			return wethTokenInstance.allowance(borrower1, spotInstance.address);
+		}).then(function(data) {
+			assert.equal(data, 10, "weth allowance amount should be " + 10);
 		});
 	});
 
-	// it("should lock", function() {
-	// 	return spotInstance.Lock(
-	// 		x1TokenInstance.address, 
-	// 		lender1, 
-	// 		100,
-	// 		0,
-	// 		10000,
-	// 		{from: borrower1, value: 10}
-	// 	).then(function(data) {
-	// 		return spotInstance.getLock(x1TokenInstance.address, lender1, borrower1);
-	// 	}).then(function(data) {
-	// 		assert.equal(data[0], 10, "amount of locked X1 should be 10");
-	// 		assert.equal(data[1], 10, "amount of locked Ether should be 10");
-	// 		assert.equal(web3.eth.getBalance(spotInstance.address), 10, "this contract shoul have 10 balance of Ether");
-	// 		return x1TokenInstance.balanceOf(borrower1);
-	// 	}).then(function(data) {
-	// 		assert.equal(data, 10, "borrower should have 10 X1");
-	// 	});
-	// });
+	it("should lock", function() {
+		return spotInstance.Lock( 
+			lender1, 
+			borrower1,
+			eosTokenInstance.address,
+			10,
+			100,
+			0,
+			10000,
+			{from: borrower1}
+		).then(function(data) {
+			return spotInstance.getLock(lender1, 0);
+		}).then(function(data) {
+			assert.equal(data[3], 10, "amount of locked Eos should be 10");
+			assert.equal(data[4], 10, "amount of locked Ether should be 10");
+			return wethTokenInstance.balanceOf(borrower1);
+		}).then(function(data) {
+			assert.equal(data, 10, "borrower should have 10 Eos");
+			return WethToken.balanceOf(borrower1);
+		});
+	});
 
 	// it("should buy to cover", function() {
 	// 	x1TokenInstance.approve(
